aedafd6d77479120191560bb6d7f8fbdbd87b95a
Keginold Philip <keginoldp@hotmail.com>
2021-02-02 15:37:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,5 +68,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.map do |season, holidays|
+	holidays.map do |holiday, supplies|
+		holiday if supplies.include?("BBQ")
+	end
+end.flattern.compact
 end
